0fc8c17474966a6ad2b2d33ac12a5d5e5cd411e3
carlnikitin <carl.nikitin@khk.ee>
2019-01-14 12:29:46 +0200

    Kõik muutuvad osad on tehtud PHP'ks ja muutumatud osad jäetud HTML'iks

--- menu/index.php
+++ menu/index.php
@@ -1,5 +1,4 @@
-<?php
-echo '<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
@@ -26,6 +25,9 @@ echo '<!DOCTYPE html>
             <div class="card-header praed2">
                 Praed<br> <i class="fas fa-utensils"></i>
             </div>
+            <?php
+            echo '
+            ?>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">Sealihapada ploomide ja aprikoosiga <br>
                     sealihapada, lisand, salat, leib <span class="hinnad">4.3€</span></li>
@@ -36,7 +38,8 @@ echo '<!DOCTYPE html>
                 <li class="list-group-item">Kartulipuder <br></br>Kartul, kaste, salat, leib <span class="hinnad">2.9€</span></li>
                 <li class="list-group-item">Hakklihakaste 1/2 <br>
                     hakklihakaste, lisand, salat, leib <span class="hinnad">2.8€</span></li>
-            </ul>
+            </ul>';
+            ?>
         </div>
 
         <!--Magustoidud-->
@@ -44,11 +47,15 @@ echo '<!DOCTYPE html>
             <div class="card-header praed2">
                 Magustoidud<br> <i class="fas fa-utensil-spoon"></i>
             </div>
+            <?php
+            echo '
+            ?>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">Rosinakissell vahukoorega <span class="hinnad">1.2€</span></li>
                 <li class="list-group-item">Kuivatatud puuviljad <span class="hinnad">1.6€</span></li>
                 <li class="list-group-item">Moosisai <span class="hinnad">0.4€</span></li>
-            </ul>
+            </ul>';
+            ?>
         </div>
 
         <!--Joogid-->
@@ -56,16 +63,19 @@ echo '<!DOCTYPE html>
             <div class="card-header praed2">
                 Joogid<br> <i class="fas fa-coffee"></i>
             </div>
+            <?php
+            echo '
+            ?>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">Mahl <span class="hinnad">0.6€</span></li>
                 <li class="list-group-item">Piim <span class="hinnad">0.4€</span></li>
                 <li class="list-group-item">Koolipiim <span class="hinnad">0.3€</span></li>
                 <li class="list-group-item">Keefir <span class="hinnad">0.4€</span></li>
                 <li class="list-group-item">Morss <span class="hinnad">0.2€</span></li>
-            </ul>
+            </ul>';
+            ?>
         </div>
     </div>
 
 </body>
-</html>';
-?>
+</html>'
